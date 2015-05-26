f4b3217092d8955d385c6c8daa0d08534d50650d
Shaun Chua <shon3005@gmail.com>
2015-05-26 18:28:07 +0800

    fourth commit

--- FibonacciApp/FibonacciSequence.swift
+++ FibonacciApp/FibonacciSequence.swift
@@ -6,14 +6,12 @@
 //  Copyright (c) 2015 Shaun Chua. All rights reserved.
 //
 
-import Foundation
-
 class FibonacciSequence {
     
     let includesZero: Bool
-    let values: [UInt]
+    let values: [UInt64]
     
-    init(maxNumber: UInt, includesZero: Bool) {
+    init(maxNumber: UInt64, includesZero: Bool) {
         self.includesZero = includesZero
         
         if maxNumber == 0 && includesZero == false {
@@ -23,13 +21,13 @@ class FibonacciSequence {
             values = [0]
         }
         else {
-            var sequence:[UInt] = [0,1,1]
-            var nextNumber:UInt = 2
+            var sequence:[UInt64] = [0,1,1]
+            var nextNumber:UInt64 = 2
             while nextNumber <= maxNumber {
                 sequence.append(nextNumber)
                 let lastNumber = sequence.last!
                 let secondToLastNumber = sequence[sequence.count-2]
-                let (sum, didOverflow) = UInt.addWithOverflow(lastNumber, secondToLastNumber)
+                let (sum, didOverflow) = UInt64.addWithOverflow(lastNumber, secondToLastNumber)
                 if didOverflow == true {
                     println("Overflow! The next number is too large!")
                     break
@@ -43,7 +41,7 @@ class FibonacciSequence {
         }
     }
     
-    init(numberOfItemsInSequence: UInt, includesZero: Bool) {
+    init(numberOfItemsInSequence: UInt64, includesZero: Bool) {
         self.includesZero = includesZero
         
         if numberOfItemsInSequence == 0 {
@@ -58,7 +56,7 @@ class FibonacciSequence {
             }
         }
         else {
-            var sequence: [UInt]
+            var sequence: [UInt64]
             if includesZero == true {
                 sequence = [0,1]
             }
@@ -68,7 +66,7 @@ class FibonacciSequence {
             for var i = 2; i < Int(numberOfItemsInSequence); i++ {
                 let lastNumber = sequence.last!
                 let secondToLastNumber = sequence[sequence.count-2]
-                let (sum, didOverflow) = UInt.addWithOverflow(lastNumber, secondToLastNumber)
+                let (sum, didOverflow) = UInt64.addWithOverflow(lastNumber, secondToLastNumber)
                 if didOverflow == true {
                     println("Overflow! The next number is too large!")
                     break
--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -10,22 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textView:UITextView!
-    
-    @IBOutlet weak var includesZeroSwitch:UISwitch!
-    
-    @IBOutlet weak var includesZeroLabel:UILabel!
-    
-    @IBOutlet weak var numberOfItemsLabel:UILabel!
-    
-    @IBOutlet weak var numberOfItemsSlider:UISlider!
-    
     var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var includesZeroSwitch: UISwitch!
+    
+    @IBOutlet weak var includesZeroLabel: UILabel!
+
+    @IBOutlet weak var numberOfItemsSlider: UISlider!
+    
+    @IBOutlet weak var numberOfItemsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.updateFibonacciSequence()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +34,7 @@ class ViewController: UIViewController {
     
     @IBAction func updateFibonacciSequence() {
         
-        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt64(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
     
         textView.text = fibonacciSequence.values.description
         
