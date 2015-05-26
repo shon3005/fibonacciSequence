1c2709fc4a0d77dbc4b1553a2c44f30eef1bac04
Shaun Chua <shon3005@gmail.com>
2015-05-26 18:29:57 +0800

    fifth commit

--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateFibonacciSequence()
     }
 
     override func didReceiveMemoryWarning() {
